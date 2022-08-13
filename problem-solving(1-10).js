3b6f2ae2856ba33c421844e94a4b88c1f2daa358
jakiasultana2022 <rokeyasbd@gmail.com>
2022-08-13 10:19:38 +0600

    first commit

---
+++ problem-solving(1-10).js
@@ -0,0 +1,69 @@
+// 1) Harry’s mom gave him tk 1000 and asked him to buy some oranges and
+// apples. Write a program to help Harry calculate how much money the
+// shopkeeper will return. The total cost of 1 kg of oranges and 1 kg of
+// apples is tk 700.
+
+
+
+// 2) Write a program to calculate the average marks of Mathematics,
+// Biology, Chemistry, Physics, and Bangla of a student.
+
+
+// 3) John’s teacher gave him two variables. Each variable contains a string.
+// John’s teacher asked him to combine these two strings(‘I am going to
+// be’ and ‘an awesome web developer’) and print them in one line. Help
+// John write the program.
+
+
+// 4) Sarah’s mother is teaching her mathematics. She gave Sarah the number
+// 119 and asked her what the remainder would be if she divided the number
+// by 5. Help Sarah write the program.
+
+
+// 5) িনেচর ĺভিরেয়বল িডেǓয়ার এ ĺকানটার মেধƟ িক িক সমসƟা আেছ। ĺদখেতা ĺবর করেত
+// পােরা িকনা?
+// Var price = 33
+// var name - Shabana
+// var boxName = ‘Cocola;
+// var 88_price = 34;
+// var enum = -1;
+// var _$box’78 = ‘Monika’;
+// var home-address = “kochu khet”;
+
+
+// 6) You are given an array:
+// var fruits = ['Apple', 'Banana', 'Orange'];a) Find the index of ‘Banana’ and replace ‘Banana’ with ‘Mango’.
+// b) Remove ‘Orange’ and add ‘Watermelon’.
+
+
+// 7) You and your friends Tom, Jane, Peter and John got their final exam
+// results. Your total score is 85, Tom’s total score is 66, Jane’s total score is
+// 95, Peter’s total score is 56 and John’s total score is 40. The grading
+// chart is
+// 80 or above A grade
+// 60 or above B grade
+// 50 or above C grade
+// 40 or above D grade
+// 39 or less => F grade
+// Write a program to find your and your friends’ grades using
+// if-else.
+
+
+// 8) You are given three numbers 13, 79, and 45. Write a program that will
+// print the largest number using if-else.
+
+
+// 9) You are given a triangle with the sides 9, 8, 9. Write a program to check
+// whether a triangle is Isosceles or not using if-else.
+// Isosceles => two sides are equal
+
+// 10) Ǔাস ĺসেভর এর ফাইনাল এǙাম এ তু িম ফাʁŪ হেয়ছ। ĺতামার বȴু আিলয়া, ডািলয়া,
+// সািলয়া, মািলয়া, িলিলয়া আর ʪালাইয়া - তােদর grade তু িম জােনা না। তেব তােদর ন˘র
+// জােনা. আিলয়া 95 ĺপেয়েছ, ডািলয়া 66 ĺপেয়েছ, সািলয়া 80 ĺপেয়েছ, মািলয়া ĺপেয়েছ 59,
+// িলিলয়া ĺপেয়েছ 47, ʪালাইয়া ĺপেয়েছ 77। তু িম JS code িদেয় তােদর grade ĺবর কের
+// িদেত পারেব?
+// ১) যারা ৫০ এর িনেচ ĺপেয়েছ, তােদর grade F.
+// ২) যারা ৫০ বা তার উপের ĺপেয়েছ, অথবা ৬০ এর িনেচ ĺপেয়েছ, তােদর grade D.
+// ৩) যারা ৬০ বা তার উপের ĺপেয়েছ, অথবা ৭০ এর িনেচ ĺপেয়েছ, তােদর grade C.৪) যারা ৭০ বা তার উপের ĺপেয়েছ, অথবা ৮০ এর িনেচ ĺপেয়েছ, তােদর grade B.
+// ৫) যারা ৮০ বা তার উপের ĺপেয়েছ, অথবা ৯০ এর িনেচ ĺপেয়েছ, তােদর grade A.
+// ৬) যারা ৯০ বা তার উপের ĺপেয়েছ, তােদর grade A+
